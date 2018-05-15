63a7cc35d31619282c51d6b89dac05e06f050d9c
yezhi <496574509@qq.com>
2018-05-15 16:22:20 +0800

    【拖拽】列表拖拽完善

--- Drag/list/listDrag.js
+++ Drag/list/listDrag.js
@@ -10,16 +10,44 @@ function Drag(obj){
 
 Drag.prototype = {
 	init:function(){
+		this.el.style.position = 'relative'
 		var _this = this
-		this.el.onmousedown = function(ev){
-			document.onmousemove = function(ev){
-				
-			}
-			document.onmouseup = function(ev){
-				document.onmousemove = null;
-				document.onmouseup = null;
+		var childs = this.el.getElementsByTagName('li')
+		var liHeight = childs[0].offsetHeight
+		for(var i=0;i<childs.length;i++){
+			drag(childs[i])
+			function drag(item){
+				item.onmousedown = function(ev){
+					var ev = ev || event
+					var oringinX = ev.clientX - ev.target.offsetLeft
+					var oringinY = ev.clientY - ev.target.offsetTop
+					
+					var blank = document.createElement('li')
+					blank.style.visibility = 'hidden'
+					blank.style.height = liHeight
+					_this.el.insertBefore(blank,ev.target.nextSibling)
+					
+					ev.target.style.position = 'absolute'
+					ev.target.style.width = '100%'
+					ev.target.style.border = '2px dashed #ccc'
+					ev.target.style.backgroundColor = '#f5f5f5'
+					
+					document.onmousemove = function(ev){
+						index = Math.ceil((ev.clientY - _this.el.offsetTop)/liHeight - 1)
+						_this.el.insertBefore(blank,childs[index])
+						
+						ev.target.style.top = ev.clientY - oringinY + 'px'
+						ev.target.style.left = ev.clientX - oringinX + 'px'
+					}
+					document.onmouseup = function(ev){
+						_this.el.insertBefore(ev.target,blank)
+						ev.target.removeAttribute('style')
+						_this.el.removeChild(blank);
+						document.onmousemove = null;
+						document.onmouseup = null;
+					}
+				}
 			}
 		}
-		
 	}
 }
